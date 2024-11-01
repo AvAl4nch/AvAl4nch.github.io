78cc49c69b962f9572f86a203d7e60d5ce0751ca
AvAl4nch <129538666+AvAl4nch@users.noreply.github.com>
2024-11-01 23:04:46 +0300

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -115,7 +115,7 @@ const CONFIG = {
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'medium', // medium | dev
-    username: 'laithsalman2522003', // to hide blog section, keep it empty
+    username: '@laithsalman2522003', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
