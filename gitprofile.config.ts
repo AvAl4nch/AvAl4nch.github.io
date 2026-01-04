7795f5ecb4251fe81f75eaf248f0f74eab0d601a
AvAl4nch <laithsalman2522003@gmail.com>
2026-01-04 10:47:13 +0300

    default themm

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -135,7 +135,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'night',
+    defaultTheme: 'forest',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
